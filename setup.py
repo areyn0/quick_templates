ca156849160aee133a731613413ea0889ddb1568
areyn0 <79251867+areyn0@users.noreply.github.com>
2021-06-14 11:52:41 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='quick_templates', 
+    version='1.0', 
+    packages=find_packages(), 
+    description = 'quickly create templates on Pug, Twig, Blade',
